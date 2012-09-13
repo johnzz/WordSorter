5f61ea1b285de047753b71d53fb5cfc861eea619
Jon Yao <corejava@126.com>
2012-09-13 14:56:12 +0800

    Update the Java code for test

--- Java/Sort.java
+++ Java/Sort.java
@@ -1,217 +1,288 @@
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Date;
 import java.util.LinkedList;
 
 final public class Sort {
 
-    private static Sort instance = null;
-    private int numberOfWords = 0;
-    private String[] words;
+	private static Sort instance = null;
+	private int numberOfWords = 0;
+	private String[] words;
 
+	// Map<String, Integer> tree = new TreeMap<String, Integer>();
 
-    public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Wrong number of arguments!");
-            return;
-        }
+	public static void main(String[] args) {
 
-        int threadCount;
-        try {
-            threadCount = Integer.parseInt(args[0]);
+		if (args.length != 3) {
+			// System.out.println("Wrong number of arguments!");
+			// return;
+			args = new String[3];
+			args[0] = "16";
+			args[1] = "sowpods.txt";
+			// args[1] = "test.txt";
+			args[2] = "out.txt";
+		}
 
-            if (threadCount < 1) {
-                System.out.println("You must specify minimum 1 thread for sorting!");
-                return;
-            }
-        } catch (NumberFormatException e) {
-            System.out.println(args[0] + " should be a number representing "
-                               + "amount of threads to be used for sorting!");
-            return;
-        }
+		int threadCount;
+		try {
+			threadCount = Integer.parseInt(args[0]);
 
-        Sort sort = Sort.getInstance();
-        Date start = new Date(); // For å beregne tidsforbruk
+			if (threadCount < 1) {
+				System.out
+						.println("You must specify minimum 1 thread for sorting!");
+				return;
+			}
+		} catch (NumberFormatException e) {
+			System.out.println(args[0] + " should be a number representing "
+					+ "amount of threads to be used for sorting!");
+			return;
+		}
 
-        if (!sort.getWordsFromInput(args[1])) { // Les inn fra fil
-            System.out.println("Could not retrieve words from " + args[1] + "!");
-            return;
-        }
+		Sort sort = Sort.getInstance();
+		Date start = new Date(); // For å beregne tidsforbruk
 
-        if (!sort.sortWords(threadCount)) { // Sorter med threadCount antall tråder
-            System.out.println("Something went wrong when sorting the words!");
-            return;
-        }
+		if (!sort.getWordsFromInput(args[1])) { // Les inn fra fil
+			System.out
+					.println("Could not retrieve words from " + args[1] + "!");
+			return;
+		}
 
-        if (!sort.writeWordsToOutput(args[2])) { // Skriv resultat til fil
-            System.out.println("Could not write sorted words to " + args[2]);
-            return;
-        }
+		if (!sort.sortWords(threadCount)) { // Sorter med threadCount antall
+											// tråder
+			System.out.println("Something went wrong when sorting the words!");
+			return;
+		}
 
-        Date end = new Date(); // Beregn tidsforbruk
-        long timeToComplete = end.getTime() - start.getTime();
+		if (!sort.writeWordsToOutput(args[2])) { // Skriv resultat til fil
+			System.out.println("Could not write sorted words to " + args[2]);
+			return;
+		}
 
-        System.out.println();
-        System.out.println("Using " + threadCount + " threads, "
-                           + sort.getWords().length + " words was sorted in "
-                           + timeToComplete + " milliseconds.");
-    }
+		Date end = new Date(); // Beregn tidsforbruk
+		long timeToComplete = end.getTime() - start.getTime();
 
-    private Sort() {} // Gjør det umulig å opprette ett nyt Sort objekt utenfor denne klassen
+		System.out.println();
+		System.out.println("Using " + threadCount + " threads, "
+				+ sort.getWords().length + " words was sorted in "
+				+ timeToComplete + " milliseconds.");
+	}
 
-    private static Sort getInstance() {
-        if (instance == null) {
-            instance = new Sort();
-        }
+	private Sort() {
+	} // Gjør det umulig å opprette ett nyt Sort objekt utenfor denne klassen
 
-        return instance;
-    } // Returner singleton objekt
+	private static Sort getInstance() {
+		if (instance == null) {
+			instance = new Sort();
+		}
 
+		return instance;
+	} // Returner singleton objekt
 
-    public boolean getWordsFromInput(String inputFile) {
-        System.out.print("Loading contents of " + inputFile + "... ");
-        Date start = new Date();
+	public boolean getWordsFromInput(String inputFile) {
+		System.out.print("Loading contents of " + inputFile + "... ");
+		Date start = new Date();
 
-        StringBuilder firstLine = new StringBuilder(); // Første linja, som inneholder antall ord
-        StringBuilder lines = new StringBuilder(); // Ordene for sortering
+		StringBuilder firstLine = new StringBuilder(); // Første linja, som
+														// inneholder antall ord
+		StringBuilder lines = new StringBuilder(200); // Ordene for sortering
 
-        try {
-            BufferedReader input = new BufferedReader(new FileReader(inputFile));
-            boolean readFirstLine = false; // Har vi lest første linje?
+		try {
+			int bufferSize = 200 * 1024;
+			ByteBuffer buffer = ByteBuffer.allocateDirect(bufferSize);
+			FileInputStream in = new FileInputStream(inputFile);// 指定目标文件
+			FileChannel channel = in.getChannel(); // 从文件中获取一个通道
+			byte[] bytes = new byte[buffer.capacity()];
+			boolean readFirstLine = false;
+			int word_num = 0;
+			while (channel.read(buffer) != -1) {
+				buffer.flip();
+				int len = buffer.remaining();
+				buffer.get(bytes, 0, len);
+				int index = 0;
+				// 首行读取单词数
+				if (!readFirstLine) {
+					for (int i = 0; i < len; i++) {
+						char readChar = (char) bytes[i];
+						if (readChar == '\r') { // ignorer \r tegn
+						} else {
+							if (readChar == '\n') {
+								readFirstLine = true;
+								index = i + 1;
+								// 已经知道多少行了
+								words = new String[Integer.parseInt(firstLine
+										.toString())];
+								break;
+							}
+							firstLine.append(readChar);
+						}
+					}
+				}
+				for (int i = index; i < len; i++) {
+					char readChar = (char) bytes[i];
+					if (readChar == '\r') {
+					} else if (readChar == '\n') {
+						words[word_num] = lines.toString();
+						lines.setLength(0);
+						word_num++;
+					} else {
+						lines.append(readChar);
+					}
 
-            for (int charByte = input.read(); charByte >= 0; charByte = input.read()) {
-                char readChar = (char)charByte;
+				}
 
-                if (readChar == '\r') { // ignorer \r tegn
-                } else if (readFirstLine) { // Vi har lest første linje
-                    lines.append(readChar);
-                } else { // Vi har ikke lest første linje, les frem til linjeskift
-                    if (readChar == '\n') {
-                        readFirstLine = true;
-                        continue;
-                    }
+				buffer.clear();
+			}
+		} catch (IOException e) {
+			return false;
+		}
 
-                    firstLine.append(readChar);
-                }
-            }
+		numberOfWords = words.length;
+		Date end = new Date();
+		long timeDiff = end.getTime() - start.getTime();
 
-            input.close();
-        } catch (IOException e) {
-            return false;
-        }
+		System.out.println(timeDiff + "ms");
 
-        words = lines.toString().split("\n");
+		return true;
+	}
 
-        try {
-            numberOfWords = Integer.parseInt(firstLine.toString());
-        } catch (NumberFormatException e) {
-            return false;
-        }
+	public boolean writeWordsToOutput(String outputFile) {
+		System.out.print("Writing results to " + outputFile + "... ");
+		Date start = new Date();
 
-        Date end = new Date();
-        long timeDiff = end.getTime() - start.getTime();
+		if (words.length != numberOfWords) { // Sjekker om vi har sortert riktig
+												// antall ord
+			System.out
+					.println("Sorted list does not contain expected number of words!");
+			return false;
+		}
 
-        System.out.println(timeDiff + "ms");
+		try {
+			int bufferSize = 400 * 1024;
+			File file = new File(outputFile);
+			if (!file.exists()) {
+				file.createNewFile();
+			} else {
+				file.delete();
+				file.createNewFile();
+			}
+			RandomAccessFile out = new RandomAccessFile(outputFile, "rw");// 指定目标文件
+			FileChannel channel = out.getChannel(); // 从文件中获取一个通道
+			ByteBuffer buffer = ByteBuffer.allocateDirect(bufferSize + 200);
+			StringBuffer buf = new StringBuffer();
+			int index = 0;
+			for (int i = 0; i < words.length; i++) {
+				// String outputWord = (i == words.length - 1) ? words[i]
+				// : words[i] + "\n";
+				buf.append(words[i]);
+				if (buf.length() >= bufferSize) {
+					buffer = channel.map(FileChannel.MapMode.READ_WRITE, index,
+							buf.length());
+					buffer.put(buf.toString().getBytes());
+					index = index + buf.length();
+					buf.setLength(0);
+					buffer.clear();
+				}
+			}
+			buffer = channel.map(FileChannel.MapMode.READ_WRITE, index,
+					buf.length());
+			buffer.put(buf.toString().getBytes());
+			channel.close();
+			buffer.clear();
 
-        return true;
-    }
+			// FileOutputStream output = new FileOutputStream(outputFile);
+			// BufferedOutputStream bw = new BufferedOutputStream(output);
+			// for (int i = 0; i < words.length; i++) {
+			// String outputWord = (i == words.length - 1) ? words[i]
+			// : words[i] + "\n";
+			// bw.write(outputWord.getBytes());
+			// }
+			// bw.flush();
+			// output.flush();
+			// bw.close();
+			// output.close();
 
-    public boolean writeWordsToOutput(String outputFile) {
-        System.out.print("Writing results to " + outputFile + "... ");
-        Date start = new Date();
+		} catch (IOException e) {
+			return false;
+		}
 
-        if (words.length != numberOfWords) { // Sjekker om vi har sortert riktig antall ord
-            System.out.println("Sorted list does not contain expected number of words!");
-            return false;
-        }
+		Date end = new Date();
+		long timeDiff = end.getTime() - start.getTime();
+		System.out.println(timeDiff + "ms");
 
-        try {
-            FileWriter output = new FileWriter(outputFile);
+		return true;
+	}
 
-            for (int i = 0; i < words.length; i++) {
-                String outputWord = (i == words.length - 1) ? words[i] : words[i] + "\n";
-                output.write(outputWord);
-            }
+	public boolean sortWords(int threadCount) {
+		System.out.print("Sorting... ");
+		Date start = new Date();
 
-            output.close();
-        } catch (IOException e) {
-            return false;
-        }
+		LinkedList<WordHandler> wordHandlers = new LinkedList<WordHandler>();
 
-        Date end = new Date();
-        long timeDiff = end.getTime() - start.getTime();
-        System.out.println(timeDiff + "ms");
+		initSortThreads(threadCount, wordHandlers); // Start sortering
+		boolean sortResult = interleaveThreads(wordHandlers); // Flett sammen
+																// resultat
 
-        return true;
-    }
+		Date end = new Date();
+		long timeDiff = end.getTime() - start.getTime();
+		System.out.println(timeDiff + "ms");
 
+		return sortResult;
+	}
 
-    public boolean sortWords(int threadCount) {
-        System.out.print("Sorting... ");
-        Date start = new Date();
+	private void initSortThreads(int threadCount,
+			LinkedList<WordHandler> wordHandlers) {
+		int wordsPerThread = words.length / threadCount;
+		int additionalWordsPerThread = words.length % threadCount;
 
-        LinkedList<WordHandler> wordHandlers = new LinkedList<WordHandler>();
+		int currentOffset = 0;
 
-        initSortThreads(threadCount, wordHandlers); // Start sortering
-        boolean sortResult = interleaveThreads(wordHandlers); // Flett sammen resultat
+		for (int i = 0; i < threadCount; i++) {
+			int wordsForThread = wordsPerThread;
 
-        Date end = new Date();
-        long timeDiff = end.getTime() - start.getTime();
-        System.out.println(timeDiff + "ms");
+			if (additionalWordsPerThread > 0) {
+				wordsForThread++;
+				additionalWordsPerThread--;
+			}
 
-        return sortResult;
-    }
+			WordSorter sorter = new WordSorter(words, currentOffset,
+					currentOffset + wordsForThread);
+			wordHandlers.add(sorter);
 
-    private void initSortThreads(int threadCount, LinkedList<WordHandler> wordHandlers) {
-        int wordsPerThread = words.length / threadCount;
-        int additionalWordsPerThread = words.length % threadCount;
+			currentOffset += wordsForThread;
+		}
+	}
 
-        int currentOffset = 0;
+	private boolean interleaveThreads(LinkedList<WordHandler> wordHandlers) {
+		WordHandler buffer = null;
 
-        for (int i = 0; i < threadCount; i++) {
-            int wordsForThread = wordsPerThread;
+		while (wordHandlers.size() > 0) {
+			try {
+				wordHandlers.peek().join();
 
-            if (additionalWordsPerThread > 0) {
-                wordsForThread++;
-                additionalWordsPerThread--;
-            }
+				if (buffer == null && wordHandlers.size() == 1) {
+					words = wordHandlers.poll().getWords();
+				} else if (buffer == null) {
+					buffer = wordHandlers.poll();
+				} else {
+					Interleaver merge = new Interleaver(buffer.getWords(),
+							wordHandlers.poll().getWords());
+					wordHandlers.add(merge);
+					buffer = null;
+				}
+			} catch (InterruptedException e) {
+				System.out.println("Main sort thread was interupted!");
+				return false;
+			}
+		}
 
-            WordSorter sorter = new WordSorter(words, currentOffset, currentOffset + wordsForThread);
-            wordHandlers.add(sorter);
+		return true;
+	}
 
-            currentOffset += wordsForThread;
-        }
-    }
-
-    private boolean interleaveThreads(LinkedList<WordHandler> wordHandlers) {
-        WordHandler buffer = null;
-
-        while (wordHandlers.size() > 0) {
-            try {
-                wordHandlers.peek().join();
-
-                if (buffer == null && wordHandlers.size() == 1) {
-                    words = wordHandlers.poll().getWords();
-                } else if (buffer == null) {
-                    buffer = wordHandlers.poll();
-                } else {
-                    Interleaver merge = new Interleaver(buffer.getWords(), wordHandlers.poll().getWords());
-                    wordHandlers.add(merge);
-                    buffer = null;
-                }
-            } catch (InterruptedException e) {
-                System.out.println("Main sort thread was interupted!");
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-
-    public String[] getWords() {
-        return words;
-    }
+	public String[] getWords() {
+		return words;
+	}
 }
--- Java/WordSorter.java
+++ Java/WordSorter.java
@@ -1,34 +1,54 @@
+import java.util.Arrays;
+import java.util.SortedSet;
+
 public class WordSorter extends Thread implements WordHandler {
 
-    private String[] sorted = null; // Den etterhvert sorterte arrayen
-    private String[] sourceStrings = null; // Kildearrayet som vi henter ordene våre fra
-    private int start = 0; // Settes i konstruktør, startindex i kildearrayet
-    private int end = 0; // Siste index i kildearrayet
+	private String[] sorted = null; // Den etterhvert sorterte arrayen
+	private String[] sourceStrings = null; // Kildearrayet som vi henter ordene
+											// våre fra
+	private int start = 0; // Settes i konstruktør, startindex i kildearrayet
+	private int end = 0; // Siste index i kildearrayet
 
+	public WordSorter(String[] sourceList, int start, int end) {
+		this.sourceStrings = sourceList;
+		this.start = start;
+		this.end = end;
 
-    public WordSorter(String[] sourceList, int start, int end) {
-        this.sourceStrings = sourceList;
-        this.start = start;
-        this.end = end;
+		start();
+	}
 
-        start();
-    }
+	@Override
+	public void run() {
+		// SortedStringsList sorter = new SortedStringsList();
+		//
+		// for (int i = start; i < end; i++) {
+		// sorter.add(sourceStrings[i]);
+		// }
+		//
+		// sorted = sorter.toArray();
 
+		// sorted = new String[end - start];
+		// System.arraycopy(sourceStrings, start, sorted, 0, end - start);
+		// Arrays.sort(sorted);
+		
+		
+		 SortedSet<String> list = new java.util.TreeSet<String>();
+		 for (int i = start; i < end; i++) {
+		 list.add(sourceStrings[i]);
+		 }
+		 sorted = new String[list.size()];
+		 list.toArray(sorted);
+//		
+		
+//		sorted = new String[tt.length];
+//		for(int i = 0;i<tt.length;i++){
+//			sorted[i]=tt[i].toString();
+//		}
+		// System.arraycopy(tt, start, sorted, 0, end - start);
 
-    @Override
-    public void run() {
-        SortedStringsList sorter = new SortedStringsList();
+	}
 
-        for (int i = start; i < end; i++) {
-            sorter.add(sourceStrings[i]);
-        }
-
-        sorted = sorter.toArray();
-    }
-
-
-    public String[] getWords() {
-        return sorted;
-    }
+	public String[] getWords() {
+		return sorted;
+	}
 }
-
